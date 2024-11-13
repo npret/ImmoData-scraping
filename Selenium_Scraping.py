786adba678bfe644418d3175b78a76dfc206230b
Kevin Potter <kvn.potter@outlook.com>
2024-11-13 10:50:51 +0100

    Updated Selenium Functions

--- Selenium_Scraping.py
+++ Selenium_Scraping.py
@@ -30,3 +30,35 @@ search_advanced.click()
 # Close driver
 
 driver.close()
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import time
+
+# Set up Chrome options
+options = Options()
+options.add_argument('--headless')  # Run in headless mode if needed
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+service = Service('C:/Program Files/Google/Chrome/Application/chrome.exe')  # Replace with your chromedriver path
+driver = webdriver.Chrome(service=service, options=options)
+
+# Open the website
+driver.get('https://www.immoweb.be')
+
+# Wait for the page to load and cookies to be set
+time.sleep(5)
+
+### HANDLE COOKIE CONSENT BANNER
+
+# Get cookies from the driver and format them for `requests`
+cookies = {cookie['name']: cookie['value'] for cookie in driver.get_cookies()}
+
+# Close the browser
+driver.quit()
+
+# Print or use the cookies in your `requests` call
+print(cookies)
