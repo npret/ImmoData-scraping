8b8db284d19ded507c03912333ae82f7350af76a
Kevin Potter <kvn.potter@outlook.com>
2024-11-15 11:42:06 +0100

    Fixing Git

--- ImmoWebScraper/main.py
+++ ImmoWebScraper/main.py
@@ -9,6 +9,15 @@ from ImmoWebScraper.CreateDataFrame import parse_listing_info
 
 if __name__ == "__main__":
 
+    # Get listing URLs from search results on ImmoWeb, search page is hard-coded in ImmoWebScraper.GetListingURLs.get_url_list. For a full scrape, page number should = 333, here only a subset of the total number of pages is scraped for example purposes. Returns a list of search result URLs and creates a txt file containing the URLs.
+
+    number_pages = 10
+    search_results = quick_get_urls(number_pages)
+
+    # Use previous URL list to solve compound URLs (listings from the search results containing information on a compound sale), 
+
+
+
     # Define headers
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',} 
     number_pages = 333
